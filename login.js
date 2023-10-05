1f83fbf8b9ed19e9f578449baf6c37e147bceac4
chanwoo402 <128678819+chanwoo402@users.noreply.github.com>
2023-10-05 16:59:51 +0900

    Add files via upload

---
+++ login.js
@@ -0,0 +1,27 @@
+// "use strict"
+
+// const id = document.querySelector("#id");
+// const psword = document.querySelector("#psword");
+// const loginBtn = document.querySelector("#loginBtn");
+
+// loginBtn.addEventListener("click", login);
+
+// function login() {
+//     const req = {
+//         id: id.value,
+//         psword: psword.value,
+//     };
+
+
+//     console.log(req);
+//     console.log(JSON).stringify(req);
+
+
+//     fetch("/login", {
+//         body: JSON.stringify(req)
+//     })
+
+
+// }
+
+console.log("hello");
